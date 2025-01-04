9d0bffa1cfae1e5c3f499dcde51cc0ed104ce76f
Amr224 <72498648+Amr224@users.noreply.github.com>
2025-01-04 13:05:45 +0200

    Optimized folder structure, developed grid for login page.

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,23 @@ import { useState } from 'react'
 
 import './App.css'
 
-function App() {
+import {NavLink, Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
 
+// Pages
+import Login from './Pages/Login'
+import SignUp from './Pages/SignUp'
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+      </Route>
+    )
+  )
   return (
     <>
-
+    <RouterProvider router={router}/>
     </>
   )
 }
