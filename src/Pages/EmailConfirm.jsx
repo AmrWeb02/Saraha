2089e8703f08e179e149f5ff431b9cf69878c4b5
Amr224 <72498648+Amr224@users.noreply.github.com>
2025-01-09 13:32:06 +0200

    Email Confirmation page is completed.

--- src/Pages/EmailConfirm.jsx
+++ src/Pages/EmailConfirm.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
-
+import { NavLink, useLocation } from 'react-router-dom'
+import { Card, CardMedia, CardContent, CardActions, Box } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import success from '../assets/SuccessMark.png'
+import '../Components/NavMenu.css'
 const EmailConfirm = () => {
   const location = useLocation();
   const indexStart = location.pathname.lastIndexOf('/');
@@ -20,7 +23,23 @@ const EmailConfirm = () => {
     console.log("Token is sent");
   }, 500);
   return (
-    <div>EmailConfirm</div>
+    <>
+     <Box component="div" sx={{backgroundColor:"primary.main", width:"100%", height:"100vh", display:"flex", justifyContent:"center", alignItems:"center",}}>
+      <Card sx={{position:"relative",backgroundColor:"primary.light", display:"flex", flexDirection:"column", justifyContent:"center",alignItems:"center", padding:"40px 0", width: {xs:"300px",sm:"600px", md:"800px"} }}>
+       <CardMedia sx={{ width: {xs:"180px", md:"300px"}, height: {xs:"180px", md:"300px"} }} image={success} title="Check mark"/>
+       <CardContent>
+        <Typography component="h5" variant="h5" align="center" sx={{fontWeight:"bolder",}}>Email Confirmed</Typography>
+        <Typography component="p" align="center">Congratulations!</Typography>
+        <Typography component="p" align="center">You can now login to your account.</Typography>
+
+       </CardContent>
+       <CardActions>
+        <NavLink to='/' className='emailBtn' style={{marginBottom:"25px"}}>Login</NavLink>
+       </CardActions>
+       <Box component="div" sx={{backgroundColor:"primary.accent"}} style={{width:"100%", height:"50px", position:"absolute",bottom:"0px"}}></Box>
+     </Card>
+     </Box>
+    </>
   )
 }
 
