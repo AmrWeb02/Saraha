7a9e41da532bb04c287376e4392f2618e80926e4
Amr224 <72498648+Amr224@users.noreply.github.com>
2025-01-30 16:20:29 +0200

    Added missing import.

--- src/Pages/ForgotPassword.jsx
+++ src/Pages/ForgotPassword.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import { NavLink } from 'react-router-dom'
 // Components
 import { Box, Card, TextField } from '@mui/material'
 import ResetGuide from '../Components/ResetGuide.jsx'
